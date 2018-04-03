02d8f293e271a2f48c582ac0cb95cfdd2a6810ad
Terry Jin <terryjin@microsoft.com>
2018-04-03 10:11:54 +0800

    update docfx version

--- MarkdownMigration.ConsoleApp/Run.ps1
+++ MarkdownMigration.ConsoleApp/Run.ps1
@@ -62,7 +62,7 @@ if (Test-Path $outputFolder)
 New-Item $outputFolder -type directory -Force
 Push-Location $repoRoot
 
-$docFxVersion = "2.32.0"
+$docFxVersion = "2.33.2"
 
 $toolsPath = Join-Path $scriptPath "_tools"
 New-Item $toolsPath -type directory -Force
