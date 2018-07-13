55a36271e0e6e0e2b11da633cbea550b216d103e
Terry Jin <terryjin@microsoft.com>
2018-07-13 13:57:33 +0800

    fix tabcontent migration bug

--- MarkdownMigration.ConsoleApp/Run.ps1
+++ MarkdownMigration.ConsoleApp/Run.ps1
@@ -62,7 +62,7 @@ if (Test-Path $outputFolder)
 New-Item $outputFolder -type directory -Force
 Push-Location $repoRoot
 
-$docFxVersion = "2.36.2"
+$docFxVersion = "2.37.0"
 
 $toolsPath = Join-Path $scriptPath "_tools"
 New-Item $toolsPath -type directory -Force
