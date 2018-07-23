18c7cf65b9e8bebc0c3271ff541c095f65176f84
Shuang Jiang <shujia@microsoft.com>
2018-07-23 15:39:43 +0800

    add appveyor build steps

--- DfmPlugins/DfmInteractiveCodeRendererPartProvider.cs
+++ DfmPlugins/DfmInteractiveCodeRendererPartProvider.cs
@@ -13,12 +13,10 @@
 
         public DfmInteractiveCodeRendererPartProvider()
         {
-            Console.WriteLine("hhhhh");
         }
 
         public IEnumerable<IDfmCustomizedRendererPart> CreateParts(IReadOnlyDictionary<string, object> parameters)
         {
-            Console.WriteLine("here");
             object value;
             if (parameters.TryGetValue("no-interactive", out value) && true.Equals(value))
             {
--- build.ps1
+++ build.ps1
@@ -2,6 +2,9 @@ $ErrorActionPreference = 'Stop'
 $scriptHome = Split-Path $MyInvocation.MyCommand.Path
 $releaseFolder = "$scriptHome/artifacts"
 $proj = "MarkdownMigration.ConsoleApp/MarkdownMigration.ConsoleApp.csproj"
+$pluginProj = "DfmPlugins/DfmPlugins.csproj"
 
 & dotnet restore $proj
 & dotnet publish $proj -c Release -f net461 -o $releaseFolder
+& dotnet restore $pluginProj
+& dotnet publish $pluginProj -c Release -f net461 -o $releaseFolder/plugins
