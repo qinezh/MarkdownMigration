a294409fe8d77da4a250c3e1b5f114fc0f6fe21d
Shuang Jiang <shujia@microsoft.com>
2019-01-17 17:42:15 +0800

    fix build.ps1 .net version

--- build.ps1
+++ build.ps1
@@ -5,6 +5,6 @@ $proj = "MarkdownMigration.ConsoleApp/MarkdownMigration.ConsoleApp.csproj"
 $pluginProj = "DfmPlugins/DfmPlugins.csproj"
 
 & dotnet restore $proj
-& dotnet publish $proj -c Release -f net461 -o $releaseFolder
+& dotnet publish $proj -c Release -f net462 -o $releaseFolder
 & dotnet restore $pluginProj
-& dotnet publish $pluginProj -c Release -f net461 -o $releaseFolder/plugins
+& dotnet publish $pluginProj -c Release -f net462 -o $releaseFolder/plugins
